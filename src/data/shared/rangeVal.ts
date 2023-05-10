9309eab560bbeb46dd20a824549206c6a619c650
cyberfrogg <alexandersudnik.q@gmail.com>
2023-05-10 21:43:18 +0300

    backend sync

--- src/data/shared/rangeVal.ts
+++ src/data/shared/rangeVal.ts
@@ -30,6 +30,18 @@ class RangeVal {
 
         return true;
     }
+
+    Clamp = (value: number): number => {
+        if (value <= this.min) {
+            return this.min;
+        }
+
+        if (value >= this.max) {
+            return this.max;
+        }
+
+        return value;
+    }
 }
 
 export default RangeVal;
